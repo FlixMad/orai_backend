4b93ce25ef4f44fe997bfecd813219f604d24d89
maru423 <jason423@naver.com>
2024-12-22 14:29:42 +0900

    chat-service 생성 및 git push test2

---
+++ chat-service/src/test/java/com/ovengers/chatservice/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovengers.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
