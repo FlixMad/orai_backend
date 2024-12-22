74be8c956c6aa1a3ec541949b654bbd63491f4fd
maru423 <jason423@naver.com>
2024-12-22 14:13:10 +0900

    유저 서비스 생성 및 테스트

---
+++ user-service/src/test/java/com/ovengers/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovengers.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
