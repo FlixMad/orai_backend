8c879e6acce661c276c752fc0c19699b609b0f2c
maru423 <jason423@naver.com>
2024-12-22 13:32:22 +0900

    디스커버리, 게이트웨이 생성

---
+++ gateway-service/src/test/java/com/ovengers/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovengers.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
