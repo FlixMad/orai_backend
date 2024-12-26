8dcd7f2dc3456d54770db5f19674db77b956b5b4
bae6794 <qo6794@naver.com>
2024-12-26 16:22:59 +0900

    유저리퀘스트디티오에  // User 엔티티로 변환하는 메서드 추가

--- user-service/src/main/java/com/ovengers/userservice/dto/UserRequestDto.java
+++ user-service/src/main/java/com/ovengers/userservice/dto/UserRequestDto.java
@@ -4,15 +4,24 @@ import com.ovengers.userservice.entity.User;
 import lombok.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Getter @Setter
+@Getter
+@Setter
 @ToString
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor // 기본 생성자 추가
 public class UserRequestDto {
     private String email;
     private String password;
     private String name;
 
-
-
+    // User 엔티티로 변환하는 메서드
+    public User toEntity(PasswordEncoder encoder) {
+        return User.builder()
+                .email(this.email)
+                .password(encoder.encode(this.password)) // 비밀번호 암호화
+                .name(this.name)
+//                .enabled(true) // 기본 활성화 상태 설정
+                .build();
+    }
 }
