1b92c36a70422dd5cb93e968fd5306ce298573f7
bae6794 <qo6794@naver.com>
2024-12-26 15:27:20 +0900

    유저리스폰스디티오 affid 주석처리

--- user-service/src/main/java/com/ovengers/userservice/dto/UserResponseDto.java
+++ user-service/src/main/java/com/ovengers/userservice/dto/UserResponseDto.java
@@ -4,11 +4,12 @@ import lombok.*;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDto {
     private String id;
     private String email;
     private String name;
-    private String affid;
+//    private String affid;
 }
