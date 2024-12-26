9dcdee2de80def46f35f7f6ab5fb4f231653c498
bae6794 <qo6794@naver.com>
2024-12-26 15:26:55 +0900

    유저레포지토리 메서드추가

--- user-service/src/main/java/com/ovengers/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/ovengers/userservice/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.ovengers.userservice.repository;
 import com.ovengers.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
+    List<User> findByIdIn(List<Long> ids);
 }
+
