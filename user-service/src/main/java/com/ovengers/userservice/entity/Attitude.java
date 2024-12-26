8a363c69e776efe013498b97c8ee10bd2d350be9
bae6794 <qo6794@naver.com>
2024-12-26 09:18:41 +0900

    에티튜드에 테이블메서드 수정 및 컬럼 추가

--- user-service/src/main/java/com/ovengers/userservice/entity/Attitude.java
+++ user-service/src/main/java/com/ovengers/userservice/entity/Attitude.java
@@ -2,10 +2,12 @@ package com.ovengers.userservice.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "attitude")
+@Table(name = "tbl_attitude")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,17 +15,18 @@ import java.time.LocalDateTime;
 @Builder
 public class Attitude {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "attitude_id")
-    private Long attitudeId; // 근태 아이디
+    private String attitudeId; // 근태 아이디
 
+    @CreationTimestamp
     @Column(nullable = false)
     private LocalDateTime createdAt; // 생성일자
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "check_in_time")
     private LocalDateTime checkInTime; // 출근 시간
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "check_out_time")
     private LocalDateTime checkOutTime; // 퇴근 시간
 
     // User와의 관계 (N:1)
