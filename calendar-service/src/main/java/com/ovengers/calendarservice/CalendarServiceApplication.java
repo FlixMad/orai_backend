7df6f90f75b4ce534dab8dbe88440bbd9d2500fc
wonsrc <96459910+wonsrc@users.noreply.github.com>
2024-12-26 16:46:43 +0900

    calendar 서비스 패키지 구성, schedule 엔티티 작성

--- calendar-service/src/main/java/com/ovengers/calendarservice/CalendarServiceApplication.java
+++ calendar-service/src/main/java/com/ovengers/calendarservice/CalendarServiceApplication.java
@@ -2,8 +2,10 @@ package com.ovengers.calendarservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CalendarServiceApplication {
 
 	public static void main(String[] args) {
