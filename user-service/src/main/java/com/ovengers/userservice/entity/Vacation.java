64266f11429dfb0e46d5987b26b4420d7da1539d
bae6794 <qo6794@naver.com>
2024-12-26 09:19:45 +0900

    베케이션에 테이블어노테이션 수정

--- user-service/src/main/java/com/ovengers/userservice/entity/Vacation.java
+++ user-service/src/main/java/com/ovengers/userservice/entity/Vacation.java
@@ -5,33 +5,28 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "vacation")
+@Table(name = "tbl_vacation")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Vacation {
+public class Vacation extends BaseTimeEntity{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "vacation_id")
-    private Long vacationId; // 연차 아이디
+    private String vacationId; // 연차 아이디
 
     @Column(nullable = false)
     private String type; // 연차 유형 (예: 연차, 반차 등)
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "start_date")
     private LocalDateTime startDate; // 연차 시작일
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "end_date")
     private LocalDateTime endDate; // 연차 종료일
-
-    @Column(nullable = false)
-    private LocalDateTime createdAt; // 생성일자
-
-    private LocalDateTime updatedAt; // 수정일자
-
-    @Column(nullable = false)
+// 아모른직다
+    @Column(nullable = false, columnDefinition = "boolean default false")
     private Boolean vacationPermission; // 연차 승인 여부
 
     // User와의 관계 (N:1)
