ebc141d7d28695a5d354ac8759813d44b57de1de
bae6794 <qo6794@naver.com>
2024-12-30 17:20:46 +0900

    UserResponseDto 수정

--- user-service/src/main/java/com/ovengers/userservice/dto/UserResponseDto.java
+++ user-service/src/main/java/com/ovengers/userservice/dto/UserResponseDto.java
@@ -12,10 +12,22 @@ public class UserResponseDto {
     private String id;
     private String email;
     private String name;
+    private String departmentId;  // 부서 ID 추가
+    private String token;         // JWT 토큰 추가
 
-    //    private String affid;
     public UserResponseDto(User user) {
+        this.id = user.getUserId();
         this.email = user.getEmail();
         this.name = user.getName();
+        this.departmentId = user.getDepartmentId();  // 부서 ID 설정
+    }
+
+    // JWT 토큰을 포함하는 생성자 추가
+    public UserResponseDto(User user, String token) {
+        this.id = user.getUserId();
+        this.email = user.getEmail();
+        this.name = user.getName();
+        this.departmentId = user.getDepartmentId();  // 부서 ID 설정
+        this.token = token;  // JWT 토큰 설정
     }
 }
