85ecd9af5c8420c490db368f7af00b96f130bbce
sup226 <rlaalswl1102@naver.com>
2024-12-30 10:23:12 +0900

    채팅방

--- chat-service/src/main/java/com/ovengers/chatservice/controller/ChatController.java
+++ chat-service/src/main/java/com/ovengers/chatservice/controller/ChatController.java
@@ -1,10 +1,9 @@
 package com.ovengers.chatservice.controller;
 
 import com.ovengers.chatservice.common.dto.CommonResDto;
-import com.ovengers.chatservice.dto.ChatMessageDto;
+import com.ovengers.chatservice.dto.MessageDto;
 import com.ovengers.chatservice.dto.ChatRoomDto;
 import com.ovengers.chatservice.dto.ChatRoomRequestDto;
-import com.ovengers.chatservice.entity.ChatRoom;
 import com.ovengers.chatservice.service.ChatRoomService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "ChatController", description = "채팅 관련 controller")
+@Tag(name = "ChatController", description = "채팅방 관련 controller")
 public class ChatController {
 
     private final SimpMessageSendingOperations template;
@@ -56,19 +55,19 @@ public class ChatController {
         return new ResponseEntity(commonResDto, HttpStatus.OK);
     }
 
-    // 채팅 리스트 반환
+/*    // 채팅 리스트 반환
     @GetMapping("/chat/{id}")
-    public ResponseEntity<List<ChatMessageDto>> getChatMessages(@PathVariable Long id){
+    public ResponseEntity<List<MessageDto>> getChatMessages(@PathVariable Long id){
         //임시로 리스트 형식으로 구현, 실제론 DB 접근 필요
-        ChatMessageDto test = new ChatMessageDto(1L, "test", "test");
+        MessageDto test = new MessageDto(1L, "test", "test");
         return ResponseEntity.ok().body(List.of(test));
     }
 
     //메시지 송신 및 수신, /pub가 생략된 모습. 클라이언트 단에선 /pub/message로 요청
     @MessageMapping("/message")
-    public ResponseEntity<Void> receiveMessage(@RequestBody ChatMessageDto chat) {
+    public ResponseEntity<Void> receiveMessage(@RequestBody MessageDto chat) {
         // 메시지를 해당 채팅방 구독자들에게 전송
         template.convertAndSend("/sub/chatroom/1", chat);
         return ResponseEntity.ok().build();
-    }
+    }*/
 }
