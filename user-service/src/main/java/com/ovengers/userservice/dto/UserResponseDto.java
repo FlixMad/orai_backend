f97fee6bc1ce11b18f4ce8963350e83e96878081
bae6794 <qo6794@naver.com>
2024-12-31 18:27:27 +0900

    유저리스폰스디티오 수정

--- user-service/src/main/java/com/ovengers/userservice/dto/UserResponseDto.java
+++ user-service/src/main/java/com/ovengers/userservice/dto/UserResponseDto.java
@@ -9,14 +9,15 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDto {
-    private String id;
+    private String userId;           // userId로 이름 변경 (User 엔티티에서 가져오기)
     private String email;
     private String name;
-    private String departmentId;  // 부서 ID 추가
-    private String token;         // JWT 토큰 추가
+    private String departmentId;     // 부서 ID 추가
+    private String token;            // JWT 토큰 추가
 
+    // User 엔티티를 받아서 변환하는 생성자
     public UserResponseDto(User user) {
-        this.id = user.getUserId();
+        this.userId = user.getUserId();  // userId로 이름 변경
         this.email = user.getEmail();
         this.name = user.getName();
         this.departmentId = user.getDepartmentId();  // 부서 ID 설정
@@ -24,10 +25,7 @@ public class UserResponseDto {
 
     // JWT 토큰을 포함하는 생성자 추가
     public UserResponseDto(User user, String token) {
-        this.id = user.getUserId();
-        this.email = user.getEmail();
-        this.name = user.getName();
-        this.departmentId = user.getDepartmentId();  // 부서 ID 설정
+        this(user);  // 기존 User 엔티티 기반 생성자를 호출
         this.token = token;  // JWT 토큰 설정
     }
 }
