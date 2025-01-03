5e4914b1159f1ca0f396d5af7154551da0f82103
sup226 <rlaalswl1102@naver.com>
2025-01-03 10:09:01 +0900

    ReactiveMongodb에 맞게 다시 설정

--- chat-service/src/main/java/com/ovengers/chatservice/mongodb/config/ReactiveMongodbConfig.java
+++ chat-service/src/main/java/com/ovengers/chatservice/mongodb/config/ReactiveMongodbConfig.java
@@ -3,7 +3,6 @@ package com.ovengers.chatservice.mongodb.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 import org.springframework.data.mongodb.core.convert.DbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
