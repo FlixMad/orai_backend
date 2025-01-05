8e1e5e479c3592e59b921bc2b8a31207b386aca0
CodeName-maru <jason423@naver.com>
2025-01-06 01:28:17 +0900

    채팅 feign 클라이언트, 리프레쉬 로직, 토큰 필터로직 작성

--- gateway-service/src/main/java/com/ovengers/gatewayservice/filter/AuthorizationHeaderFilter.java
+++ gateway-service/src/main/java/com/ovengers/gatewayservice/filter/AuthorizationHeaderFilter.java
@@ -79,7 +79,8 @@ public class AuthorizationHeaderFilter
             // 사용자 정보를 클레임에서 꺼내서 헤더에 담자.
             ServerHttpRequest request = exchange.getRequest()
                     .mutate()
-                            .header("X-User-Id", claims.getSubject())
+                    .header("X-User-Id", claims.getSubject())
+                    .header("X-User-DepartmentId", claims.get("departmentId", String.class))
                     .build();
 
             // 새롭게 만든(토큰 정보를 헤더에 담은) request를 exchange에 갈아끼워서 보내자.
