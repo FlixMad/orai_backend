2d1346c08d530f1b381806ac789d780c86601350
CodeName-maru <jason423@naver.com>
2025-01-31 14:26:44 +0900

    유레카 걷어내기 2

--- calendar-service/src/main/java/com/ovengers/calendarservice/client/EtcServiceClient.java
+++ calendar-service/src/main/java/com/ovengers/calendarservice/client/EtcServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "etc-service")
+@FeignClient(name = "etc-service", url = "http://etc-service.default.svc.cluster.local:8082")
 public interface EtcServiceClient {
     @PostMapping("api/notifications")
     ResponseEntity<?> createNotification(@RequestBody NotificationEvent event);
--- user-service/src/main/java/com/ovengers/userservice/client/CalendarServiceClient.java
+++ user-service/src/main/java/com/ovengers/userservice/client/CalendarServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
 
-@FeignClient(name = "calendar-service")
+@FeignClient(name = "calendar-service", url = "http://calendar-service.default.svc.cluster.local:8084")
 public interface CalendarServiceClient {
     @GetMapping("/api/departments/map")
     Map<String, String> getDepartmentMap();
