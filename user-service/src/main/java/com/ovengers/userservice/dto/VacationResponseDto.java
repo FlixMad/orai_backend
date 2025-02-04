11c51685ad8023a910806db2d40726ecff1a657a
bae6794 <qo6794@naver.com>
2025-02-04 19:56:46 +0900

    내가 쓴 휴가 내역 나오게 하는 기능 완료

--- user-service/src/main/java/com/ovengers/userservice/dto/VacationResponseDto.java
+++ user-service/src/main/java/com/ovengers/userservice/dto/VacationResponseDto.java
@@ -22,15 +22,17 @@ public class VacationResponseDto {
     private LocalDate endDate; // 종료일
     private VacationState vacationState; // 휴가 상태
     private String userId; // 신청자 ID
-    private String approvalId; // 결재 ID (연관된 결재)
+    private String userName;
+    private String approvalId;// 결재 ID (연관된 결재)
     // Vacation 엔티티를 기반으로 하는 생성자 추가
-    public VacationResponseDto(Vacation vacation) {
+    public VacationResponseDto(Vacation vacation, String userName) {
         this.vacationId = vacation.getVacationId();
         this.type = vacation.getType();
         this.startDate = vacation.getStartDate();
         this.endDate = vacation.getEndDate();
         this.vacationState = vacation.getVacationState();
         this.userId = vacation.getUserId();
+        this.userName = userName;
         this.approvalId = vacation.getApproval() != null ? vacation.getApproval().getApprovalId() : null;
     }
 }
