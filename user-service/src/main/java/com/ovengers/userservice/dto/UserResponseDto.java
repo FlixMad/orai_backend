0625f6c98f72962af366ad92156485bb45275845
CodeName-maru <jason423@naver.com>
2025-02-04 17:02:43 +0900

    사용자 생성에 mfa시크릿 추가

--- user-service/src/main/java/com/ovengers/userservice/dto/UserResponseDto.java
+++ user-service/src/main/java/com/ovengers/userservice/dto/UserResponseDto.java
@@ -30,7 +30,6 @@ public class UserResponseDto {
         this.position = user.getPosition();
         this.phoneNum = user.getPhoneNum();
         this.accountActive = user.isAccountActive();
-        this.departmentName = map.get(user.getDepartmentId());
         this.departmentId = map.get(user.getDepartmentId());
     }
     public UserResponseDto(User user) {
